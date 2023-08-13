a32154611d73680786e1c96fff0baa928a286fb8
Kuznetsov Sergey <monteg179@yandex.ru>
2023-08-13 09:51:19 +0300

    version 0.1

--- backend/recipes/apps.py
+++ backend/recipes/apps.py
@@ -5,5 +5,5 @@ class RecipesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipes'
 
-    def ready(self):
-        import recipes.signals
+    # def ready(self):
+    #     import recipes.signals
